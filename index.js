cf7d93116ad0c5c9166a6541e381c1fad1012c54
soyoung.kim <guana97@naver.com>
2022-11-10 02:23:27 +0900

    feat : 버튼 클릭 시 로고 및 배경 변경 기능 구현 #22

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const startBtn = document.querySelector(".startBtn");
 const lapStartCont = document.querySelector(".laptop-lock"); // 랩탑잠금화면
 const deskStartCont = document.querySelector(".desktop-lock"); // 데스크탑시작화면
+const imgStartCont = document.querySelector(".img-char"); //시작화면 나타나는 캐릭터
+const imgLogo = document.querySelector(".img-logo"); //로고
+
 startBtn.addEventListener("click", () => {
   lapStartCont.classList.add("hide");
   deskStartCont.classList.add("hide");
+  imgStartCont.classList.remove("hide");
+  imgLogo.src = "./images/logo_color.png";
+  document.querySelector("body").style.backgroundImage="url(./images/main-background.png"
 });
 
 // 잠금화면 시간 표시
